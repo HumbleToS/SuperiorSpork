de22209b5e94340988059b6f983f16e944ead245
Humble <70359789+HumbleToS@users.noreply.github.com>
2022-12-12 17:14:22 -0700

    Import Embed from discord, remove discord import

--- utils/embeds.py
+++ utils/embeds.py
@@ -1,14 +1,13 @@
 import random
 
-import discord
-from discord import Colour
+from discord import Colour, Embed
 
 
 def pastel_color():
     return Colour.from_hsv(random.random(), 0.28, 0.97)
 
 
-class SporkEmbed(discord.Embed):
+class SporkEmbed(Embed):
     def __init__(self, **kwargs):
         if kwargs.get('color', None) is None:
             kwargs['color'] = pastel_color()
