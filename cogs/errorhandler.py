ee09e95f2f65e6859d76317aa51cae65b9dac925
HumbleToS <ileakpics2020@gmail.com>
2022-12-18 13:58:37 -0700

    Added errorhandler

---
+++ cogs/errorhandler.py
@@ -0,0 +1,72 @@
+import logging
+import math
+import traceback
+
+import discord
+from discord import app_commands
+from discord.ext import commands
+
+from utils.checks import NotGuildOwner
+from utils.embeds import SporkEmbed
+
+_logger = logging.getLogger(__name__)
+
+
+class ErorrHandler(commands.Cog):
+    def __init__(self, bot: commands.Bot) -> None:
+        self.bot = bot
+
+    def cog_load(self):
+        tree = self.bot.tree
+        tree.on_error = self.on_app_command_error
+
+    def cog_unload(self):
+        tree = self.bot.tree
+        tree.on_error = tree.__class__.on_error  # type: ignore
+
+    async def on_app_command_error(self, interaction: discord.Interaction, error: app_commands.AppCommandError):
+
+        if isinstance(error, app_commands.CommandOnCooldown):
+            current_cooldown = math.floor(error.retry_after * 100) / 100
+            return await interaction.response.send_message(
+                f"This command is on cooldown for another {current_cooldown} second{'s' * (current_cooldown != 1)}!"
+            )
+        else:
+            trace = "".join(traceback.format_exception(type(error), error, error.__traceback__))
+            _logger.error("Ignoring exception in command {}:".format(interaction.command))
+            _logger.error(trace)
+
+    @commands.Cog.listener()
+    async def on_command_error(self, ctx: commands.Context, error: commands.CommandError):
+
+        if hasattr(ctx.command, "on_error"):
+            return
+
+        ignored = (commands.CommandNotFound, commands.NotOwner)
+        error = getattr(error, 'original', error)
+        command_used = ctx.invoked_with
+
+        if isinstance(error, ignored):
+            return
+
+        if isinstance(error, commands.CommandOnCooldown):
+            current_cooldown = math.floor(error.retry_after * 100) / 100
+            return await ctx.send(
+                f"You can do `{command_used}` again in {current_cooldown} second{'s' * (current_cooldown != 1)}"
+            )
+        elif isinstance(error, commands.TooManyArguments):
+            return await ctx.send(f"The command `{command_used}` was used with too many arguments")
+        elif isinstance(error, commands.UserInputError):
+            return await ctx.send(f"The command `{command_used}` was used incorrectly")
+        elif isinstance(error, commands.MissingRequiredArgument):
+            return await ctx.send(f"You're missing the required argument `{error.param.name}`")
+        elif isinstance(error, NotGuildOwner):
+            return await ctx.send(f'The command `{command_used}` can only be used by the server owner.')
+        else:
+            trace = "".join(traceback.format_exception(type(error), error, error.__traceback__))
+            _logger.error("Ignoring exception in command {}:".format(ctx.command))
+            _logger.error(trace)
+
+
+async def setup(bot: commands.Bot):
+    await bot.add_cog(ErorrHandler(bot))
