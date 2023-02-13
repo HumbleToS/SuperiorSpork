aeb92de9c2f1d262cf586b136891c32770c7cf81
HumbleToS <ileakpics2020@gmail.com>
2023-02-13 15:53:25 -0700

    Add enum for Status emojis

---
+++ exts/utils/emojis.py
@@ -0,0 +1,8 @@
+from enum import Enum
+
+
+class Status(Enum):
+    online = '<:online:1071326241375928433>'
+    idle = '<:idle:1071326224862945320>'
+    dnd = '<:dnd:1071326197331546142>'
+    offline = '<:offline:1071326256223748136>'
