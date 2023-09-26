c55e61e75290dac20652e2eb0b8d8c34fd8514be
HumbleToS <ileakpics2020@gmail.com>
2023-09-26 10:19:10 -0600

    Fix wording with plural function

--- exts/utils/time.py
+++ exts/utils/time.py
@@ -1,6 +1,8 @@
 from datetime import timedelta
 
+from .wording import plural
+
 
 def how_old(time: timedelta):
     days, hours = time.days, time.seconds // 3600
-    return f"{days:,} days and {hours:,} hours old"
+    return f"{days:,} days and {hours:,} {plural('hour', hours)} old"
