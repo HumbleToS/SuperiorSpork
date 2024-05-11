42972a8ba5082e7d2c5ce40396a2b0f15ddba6e9
HumbleToS <ileakpics2020@gmail.com>
2024-05-11 11:55:03 -0600

    Add cleanup command (thanks fret)

--- exts/general.py
+++ exts/general.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+import logging
 import os
 import re
 import time
@@ -26,6 +27,8 @@ if TYPE_CHECKING:
 
     from .utils.context import GuildContext
 
+_logger = logging.getLogger(__name__)
+
 
 class General(commands.Cog):
     def __init__(self, bot: Spork) -> None:
@@ -43,6 +46,38 @@ class General(commands.Cog):
             )
             return await message.reply(embed=embed)
         return
+
+    @commands.command(aliases=("cu", "pb"))
+    @commands.guild_only()
+    @commands.cooldown(1, 5.0, commands.BucketType.user)  # 1 per 5 seconds per user
+    async def cleanup(self, ctx: GuildContext, amount: int = 100) -> None:
+        """Purges messages from and relating to the bot.
+
+        Parameters
+        ----------
+        amount : int, optional
+            The number of messages to check (1-100), defaults to 100.
+        """
+        amount = max(min(amount, 100), 1) if not await self.bot.is_owner(ctx.author) else max(min(amount, 1000), 1)
+
+        a = ctx.author
+        c = ctx.channel
+
+        _logger.debug(f"Processing cleanup command from {a.id=} {c.id=} {amount=}")
+
+        async with ctx.typing():
+            bulk = ctx.channel.permissions_for(ctx.guild.me).manage_messages
+            try:
+                channel_prefixes = tuple(await self.bot.get_prefix(ctx.message))
+                msgs = await ctx.channel.purge(
+                    bulk=bulk,
+                    limit=amount,
+                    check=lambda m: m.author == self.bot.user or (bulk and m.content.startswith(channel_prefixes)),
+                )
+                await ctx.send(f"Removed {len(msgs)} messages.", delete_after=2.0)
+
+            except (discord.Forbidden, discord.HTTPException):
+                await ctx.send("I couldn't process this request. Please check my permissions.")
 
     @commands.hybrid_command()
     @commands.guild_only()
