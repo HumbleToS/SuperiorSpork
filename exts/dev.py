641e624f04765b9cec0e650e1c3a8e09bfb97bf5
HumbleToS <ileakpics2020@gmail.com>
2024-05-17 08:58:00 -0600

    Add sync command as jishaku's sync no longer works with discord.py

---
+++ exts/dev.py
@@ -0,0 +1,63 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING, Literal, Optional
+
+import discord
+from discord.ext import commands
+
+if TYPE_CHECKING:
+    from bot import Spork
+
+
+class Developer(commands.Cog):
+    def __init__(self, bot: Spork) -> None:
+        self.bot = bot
+
+    @commands.command()
+    @commands.guild_only()
+    @commands.is_owner()
+    async def sync(
+        self,
+        ctx: commands.Context,
+        guilds: commands.Greedy[discord.Object],
+        spec: Literal["~", "*", "^"] | None = None,
+    ) -> None:
+        """Syncs command tree.
+        Parameters
+        -----------
+        guilds: list[int]
+            The guilds to sync to
+        spec: str
+            The spec to sync.
+            ~ -> Current Guild
+            * -> Globals to current guild
+            ^ -> Clear globals copied to current guild.
+        """
+        if not guilds:
+            if spec == "~":
+                synced = await ctx.bot.tree.sync(guild=ctx.guild)
+            elif spec == "*":
+                ctx.bot.tree.copy_global_to(guild=ctx.guild)
+                synced = await ctx.bot.tree.sync(guild=ctx.guild)
+            elif spec == "^":
+                ctx.bot.tree.clear_commands(guild=ctx.guild)
+                await ctx.bot.tree.sync(guild=ctx.guild)
+                synced = []
+            else:
+                synced = await ctx.bot.tree.sync()
+            await ctx.send(f"Synced {len(synced)} commands {'globally' if spec is None else 'to the current guild.'}")
+            return
+
+        ret = 0
+        for guild in guilds:
+            try:
+                await ctx.bot.tree.sync(guild=guild)
+            except discord.HTTPException:
+                pass
+            else:
+                ret += 1
+        await ctx.send(f"Synced the tree to {ret}/{len(guilds)}.")
+
+
+async def setup(bot: Spork) -> None:
+    await bot.add_cog(Developer(bot))
