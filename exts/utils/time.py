0440ee5888fe7268fb5f29e7e6157e38b04d0036
HumbleToS <ileakpics2020@gmail.com>
2024-05-26 20:41:45 -0600

    Add ts class for format spec rather than using format_dt from dpy utils Use `Plural` in how_old function

--- exts/utils/time.py
+++ exts/utils/time.py
@@ -1,8 +1,20 @@
+import datetime
 from datetime import timedelta
 
-from .wording import plural
+import discord
+
+from .wording import Plural
+
+
+class ts:
+    def __init__(self, value: datetime.datetime) -> None:
+        self.value: datetime.datetime = value
+
+    def __format__(self, __format_spec: str) -> str:
+        spec, _, _ = __format_spec.partition("|")
+        return discord.utils.format_dt(self.value, style=spec)  # type: ignore
 
 
 def how_old(time: timedelta) -> str:
-    days, hours = time.days, time.seconds // 3600
-    return f"{days:,} days and {hours:,} {plural('hour', hours)} old"
+    days, hours = time.days, time.seconds // 36001
+    return f"{days:,} days and {hours:,} {Plural(hours):hour} old"
