33d87bfae4e793785c840afdef1e0d97c6a53183
HumbleToS <ileakpics2020@gmail.com>
2024-05-26 20:42:11 -0600

    Swap to using a Plural class to format spec in f string

--- exts/utils/wording.py
+++ exts/utils/wording.py
@@ -1,7 +1,15 @@
 from collections.abc import Sequence
+from typing import SupportsAbs
 
 
-def plural(word: str, obj: Sequence | list | float) -> str:
-    if isinstance(obj, list):
-        return f"{word}{'s' * (len(obj) != 1)}"
-    return f"{word}{'s' * (obj not in [0, 1])}"
+class Plural:
+    def __init__(self, value: SupportsAbs[int]) -> None:
+        self.value = value
+
+    def __format__(self, format_spec: str) -> str:
+        v = self.value
+        singular, _, plural = format_spec.partition("|")
+        plural = plural or f"{singular}s"
+        if abs(v) != 1:
+            return f"{v} {plural}"
+        return f"{v} {singular}"
